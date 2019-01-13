d88c512d9117da31ace2bc6ecba574a638101c7a
Константин <fix@inbox.ru>
2019-01-13 23:54:56 +0500

    Update EventClasses.cs

--- MusicXML_to_TXT/MusicXML_to_TXT/EventClasses.cs
+++ MusicXML_to_TXT/MusicXML_to_TXT/EventClasses.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,48 +6,50 @@ using System.Threading.Tasks;
 
 namespace MusicXML_to_TXT
 {
-  public class EventInfo
-  {
-    public string PitchStep;
-    public int Alter;
-    public int Octave;
-    public string Type;
-    public bool IsChord = false;
-    public bool IsRest = false;
-    public bool IsDot = false;
-    public (int actual, int normal) TimeMod;
-    public string Notehead;
-    public bool IsMeasureEnd = false;
-    public bool IsSongEnd = false;
-  }
-
-  public class DurationsToChar
-  {
-    public string Type;
-    public bool IsDot = false;
-    public (int actual, int normal) TimeMod;
-    public char Char;
-
-    public DurationsToChar() { }
-  }
-
-  public class MeasureTxt
-  {
-    public List<string> EventsTxt;
-
-    public MeasureTxt()
+    public class EventInfo
     {
-      EventsTxt = new List<string>();
+        public string PitchStep;
+        public int Alter;
+        public int Octave;
+        public string Type;
+        public bool IsChord = false;
+        public bool IsRest = false;
+        public bool IsDot = false;
+        public int TimeModActual = 0;
+        public int TimeModNormal = 0;
+        public string Notehead;
+        public bool IsMeasureEnd = false;
+        public bool IsSongEnd = false;
     }
-  }
 
-  public class SongTxt
-  {
-    public List<MeasureTxt> Measures;
-
-    public SongTxt()
+    public class DurationsToChar
     {
-      Measures = new List<MeasureTxt>();
+        public string Type;
+        public bool IsDot = false;
+        public int TimeModActual = 0;
+        public int TimeModNormal = 0;
+        public char Char;
+
+        public DurationsToChar() { }
     }
-  }
+
+    public class MeasureTxt
+    {
+        public List<string> EventsTxt;
+
+        public MeasureTxt()
+        {
+            EventsTxt = new List<string>();
+        }
+    }
+
+    public class SongTxt
+    {
+        public List<MeasureTxt> Measures;
+
+        public SongTxt()
+        {
+            Measures = new List<MeasureTxt>();
+        }
+    }
 }
